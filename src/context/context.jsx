bfb0f173550ad586bbd80345917264bc5a866749
Abdoubel26 <abdoubelounis09@gmail.com>
2026-06-23 17:47:40 +0100

    remove unused import

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 import runChat from "../config/gemini";
 import { marked } from 'marked'
 
